aabb971858e2a4d7a2481af58e3a692f2ad98fc2
Julian-dr-dev <julianmartinez21466@gmail.com>
2022-12-20 23:50:48 -0800

    initial code

--- src/emailApp/EmailApp.java
+++ src/emailApp/EmailApp.java
@@ -1,4 +1,10 @@
 package emailApp;
 
 public class EmailApp {
+
+    public static void main(String[] args)
+    {
+        Mail email = new Mail("Julian", "Martinez");
+
+    }
 }
--- src/emailApp/Mail.java
+++ src/emailApp/Mail.java
@@ -1,4 +1,30 @@
 package emailApp;
 
 public class Mail {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String dept;
+    private String altEmail;
+    private int inboxCapacity;
+
+
+    //constructor for the first & lastname
+
+    public Mail(String firstName, String lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //askfor the password
+
+    //generate random password
+
+    //set the inbox capacity
+
+    //set and set al email
+
+
+    //Change the password
 }
