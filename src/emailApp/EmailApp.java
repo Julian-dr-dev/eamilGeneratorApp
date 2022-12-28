6ae57339b0346f2ecceca02765fc7ced2596cd82
Julian-dr-dev <julianmartinez21466@gmail.com>
2022-12-27 19:13:57 -0800

    complete project

--- src/emailApp/EmailApp.java
+++ src/emailApp/EmailApp.java
@@ -1,9 +1,20 @@
 package emailApp;
-
+import java.util.Scanner;
+import java.util.*;
 public class EmailApp {
 
     public static void main(String[] args)
     {
-        Mail email = new Mail("Julian", "Martinez");
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+
+        //input first name
+        System.out.println("Please enter your first name: ");
+        String fname = sc1.next();
+
+        //input last name
+        System.out.println("Please enter your last name: ");
+        String Lname = sc2.next();
+        Mail email = new Mail(fname, Lname);
     }
 }
--- src/emailApp/Mail.java
+++ src/emailApp/Mail.java
@@ -1,14 +1,17 @@
 package emailApp;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Mail {
+    private String compnayDomain = "company.com";
     private String firstName;
     private String lastName;
     private String password;
     private String dept;
     private String altEmail;
     private int inboxCapacity;
+    private int defaultPwLen = 10;
 
 
     //constructor for the first & lastname
@@ -20,6 +23,14 @@ public class Mail {
 
         this.dept = setDept();
         System.out.println("Department is " + this.dept);
+
+        this.password = randoPassword(12);
+        System.out.println("Your password is: " + this.password);
+
+        //generate a user email
+        String userEmail = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + dept + "." + compnayDomain;
+
+        System.out.println("Your email is " + userEmail);
 
     }
 
@@ -33,13 +44,13 @@ public class Mail {
         int choice = scIn.nextInt();
         switch (choice) {
             case 1:
-                return "Sales";
+                return "sales";
             case 2:
-                return "Accounting";
+                return "accounting";
             case 3:
-                return "Devlopment";
+                return "development";
             case 4:
-                return "Human resources";
+                return "HR";
             default:
                 return "No department.";
         }
@@ -51,16 +62,40 @@ public class Mail {
         String pwSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
         char[] pw = new char[length];
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++)
+        {
             int rand = (int) (Math.random() * pwSet.length());
             pw[i] = pwSet.charAt(rand);
         }
         return new String(pw);
     }
-    //set the inbox capacity
+
+
+    //set inbox capacity
+    public void setInboxCapacity(int capacity) {
+        this.inboxCapacity = inboxCapacity;
+    }
 
     //set and set al email
-
+    public void setAltEmail(String altEmail) {
+        this.altEmail = altEmail;
+    }
 
     //Change the password
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    public int getInboxCapacity() {
+        return inboxCapacity;
+    }
+
+    public String getaltEmail() {
+        return altEmail;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+
 }
